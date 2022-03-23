3f2afa04cbd68ec4c458f90e29fb23311971be0f
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-03-23 19:55:43 +0600

    new internal JSON file, Add wo cart with specific id

--- src/Components/Cosmetic/Cosmetic.js
+++ src/Components/Cosmetic/Cosmetic.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import './cosmetic.css'
 const Cosmetic = (props) => {
+    const {name, gender, id} = props.cosmetic; //destructuring
+    
+    //add to cart with specific product id
+    const addToCart = () =>{
+        console.log('item added', id);
+    }
+
+    //calling addToCart Function with addToCartWithParam Function
+    const addToCartWithParam = () => addToCart(id);
+
     return (
-        <div>
-            <h1>Name: {props.cosmetic.name}</h1>
-            <h2>Gender: {props.cosmetic.gender} </h2>
+        <div className='product'>
+            <h1>Name: {name}</h1>
+            <h2>Gender: {gender} </h2>
+            <p>ID: {id}</p>
+            <button onClick={addToCartWithParam}>Add To Cart</button>
         </div>
     );
 };
