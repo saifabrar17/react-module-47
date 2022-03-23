d13394b5815a382544bb235373581afc1168b850
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-03-23 21:19:46 +0600

    adding some comments

--- src/Utilities/fakedb.js
+++ src/Utilities/fakedb.js
@@ -1,13 +1,13 @@
 const addToDb = (id) => {
     const quantity = localStorage.getItem(id);
     if(quantity){
-        console.log('already exist');
-        const newQuantity = parseInt(quantity)+1;
-        localStorage.setItem(id, newQuantity);
+        // console.log('already exist');
+        const newQuantity = parseInt(quantity)+1;  //local storage e item quantity barano
+        localStorage.setItem(id, newQuantity); //barano quantity localstorage e set kora
     }
     else{
-        console.log('new item');
-        localStorage.setItem(id, 1);
+        // console.log('new item');
+        localStorage.setItem(id, 1); //old data exist na korle new item set korbe
     }
 }
 
